84d8b4377962f6db7bbf6c9e7f2d2180624269a2
Philipp Otto <philipp@scm.io>
2017-05-18 18:46:23 +0200

    add a layer and sql parser extension

--- node-playground.js
+++ node-playground.js
@@ -2,6 +2,10 @@ eval(readFile('bs-js-compiler.js'))
 
 var JSParser = BSJSParser;
 
+function toJS(code) {
+  return JSParser.matchAll(code, "topLevel");
+}
+
 ometa CondJSParser <: JSParser {
    primExpr = primExpr:p
                  ( "?" "." "name":m "(" listOf(#expr, ','):as ")" -> ["call", ["get", "cond"], p, ["func", ["p"], ["send", m, ["get", "p"]]]]
@@ -37,5 +41,37 @@ ometa CombinedParser <: JSParser {
 
 a = CombinedParser.matchAll("abc?.bla!.blu", "topLevel")
 b = BSJSTranslator.match(a, "trans")
-console.log(a);
+
+
+ometa LayerParser <: JSParser {
+  stmt = "layer" "name":n layerBody:layerBody -> ["var", n, ["call", ["get", "createLayer"]].concat(layerBody)]
+         | super("stmt"),
+  layerBody = "{" classExtension*:c "}" -> c,
+  classExtension = "extends" json:json -> json
+}
+// this modifies the keywords array of the superclass
+// it might be useful to have context-aware keyword detection
+LayerParser.keywords["layer"] = true;
+LayerParser.keywords["extends"] = true;
+LayerParser.keywords["class"] = true;
+
+a = LayerParser.matchAll("layer bla { extends {someMethod: function() {}} extends {someOtherMethod: function() {}} }", "topLevel")
+// console.log(a);
+
+ometa SqlParser <: JSParser {
+  primExprHd = "sql" "{" _sqlContent:content "}" -> toJS("createSql('" + content + "')")
+               | super("primExprHd"),
+  _sqlContent = "{" _sqlContent:ch "}" _chars:chars -> ("{" + ch + "}" + chars) | _chars:ch _sqlContent:b -> ch.concat(b) | _chars,
+  _chars = _char*:chars -> chars.join(""),
+  _char =  ( ~'}' ~'{' anything ):x -> x
+}
+SqlParser.keywords["sql"] = true;
+
+a = SqlParser.matchAll("var bla = sql {{sele{ct som{e}th}ing()}}", "topLevel")
+
+
+
+console.log(a)
+
+b = BSJSTranslator.match(a, "trans")
 console.log(b);
